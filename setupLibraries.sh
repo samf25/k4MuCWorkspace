14f7dbc41b7c1cb19c84912db2e9b841ca95e04d
Samuel Ferraro <113254671+samf25@users.noreply.github.com>
2025-07-21 15:30:47 -0400

    New container has new paths

--- setupLibraries.sh
+++ setupLibraries.sh
@@ -7,11 +7,18 @@ export PYTHONPATH=${buildDir}/python:$PYTHONPATH
 export CMAKE_PREFIX_PATH=${buildDir}:$CMAKE_PREFIX_PATH
 
 
-export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/acts-32.1.0-yacvft6qr5l5ra67k7ss4mnvfbju6qza/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/vdt-0.4.4-243z3wcxk4gkd7j2jlxcrdfa27bq46az/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/acts-32.1.0-4vy43fwhiklifkcpm2nhv37sv44d7z5y/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/vdt-0.4.4-spajvmfpbzv3yllbrbg7vw26horykqxp/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/xerces-c-3.3.0-7udplkbcss57oci6gkpochnq4yatplgw/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/gsl-2.8-w5snzrtm5dmifsgmjycqz6ldj5ofuul6/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/clhep-2.4.7.1-pi7m3sbrmz5ex4audlodc5vaqc2o4zg7/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/pandorasdk-3.4.2-33sx55dznpsqceud6wlroqaru7v4cpqo/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/marlintrk-2.9.2-kibtsh7cumdyybrpvzliguwbtubqul6t/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/lccontent-3.1.6-ixnoiwljthdbi7abfwfptolewzacdleb/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/marlintrk-2.9.2-uyvlsonkn4odohpn2ntcazxejx7knyz6/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/lccontent-3.1.6-uv7ucnbyrws2zvwgrz6krejayduhyxvj/lib:$LD_LIBRARY_PATH
+
+
+
+
+
+
+
